f56ad04579fc1d260b376072f4df008e56fd52e2
MinSOh <minsoh@kaist.ac.kr>
2019-12-03 14:14:51 +0900

    chatroom: name added

--- prototypes/prototype_kakaotalk/js/chatroom.js
+++ prototypes/prototype_kakaotalk/js/chatroom.js
@@ -1,4 +1,5 @@
 var current_other = false;
+var current_name = "";
 var current_id = 0;
 
 function size_control(str){
@@ -23,20 +24,21 @@ function add_my_msg(){
     current_id = current_id + 1;
     document.getElementById('kkk').id = 'fixed';
 }
-function addMyMsgByTxt(str,isMy){
+function addMyMsgByTxt(str,who){
     var temp = document.createElement('div');
     var message = str;
     message = "<p style='word-break:break-all;'>"+str+"</p>";
-    if (isMy){
+    if (who=="me"){
         temp.innerHTML = "<div class='my_msg' id='kkk'><div class='txt' id='temp'>"+ message +"</div></div>";
         current_other = false;
     }else{
-        if (current_other){
+        if (current_other && current_name == who){
             temp.innerHTML = "<div class='other_msg' id='kkk'><div class='profile'></div><div class='txt' id='temp'>"+ message +"</div></div>";
         }
         else{
-            temp.innerHTML = "<div class='other_msg' id='kkk'><div class='profile'><img src='img/temp.jpg' alt='ProfileImage' width='40px' height='40px' /></div><div class='txt' id='temp'>"+ message +"</div></div>";
+            temp.innerHTML = "<div class='other_msg' id='kkk'><div class='profile'><img src='img/temp.jpg' alt='ProfileImage' width='40px' height='40px' /></div><div class='p_name'>"+who+"</div><div class='txt' id='temp'>"+ message +"</div></div>";
             current_other = true;
+            current_name = who;
         }
         
     }
@@ -44,7 +46,7 @@ function addMyMsgByTxt(str,isMy){
     document.getElementById('input_msg_box').value = "";
     var temp = document.getElementById('temp');
     var hhh = getComputedStyle(temp).height;
-    var qqq = parseFloat(hhh)+20.0;
+    var qqq = parseFloat(hhh)+30.0;
     hhh = qqq+"px";
     document.getElementById('kkk').style.height = hhh;
     document.getElementById('temp').id = current_id;
