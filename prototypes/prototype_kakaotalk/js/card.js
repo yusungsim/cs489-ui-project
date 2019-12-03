c789210ae7283934a5e4bd5add321a2385844eca
alexandra9975 <sandra7033@gmail.com>
2019-12-04 03:11:13 +0900

    adding parsing function

--- prototypes/prototype_kakaotalk/js/card.js
+++ prototypes/prototype_kakaotalk/js/card.js
@@ -103,3 +103,83 @@ function show_card_msg(current_obj, cnt) {
 	//current_obj = nextObj(current_obj);
 	//alert(current_obj.text());
 }
+
+var chat_log = [["윤기목", "발표 자료에 25번이랑 27번 슬라이드 내용 똑같은 거 같은데 뒤에 꺼 빼면 될까요?"],
+                ["전정연", "앗 27번 빼면될것 같아요"], ["전정연", "깜빡하고 삭제를 안했네요ㅠㅠ"], ["윤기목", "넵넵 괜찮습니다 ㅎㅎ"],
+                ["이기상", "발표자료에 오타 몇개 있어서 수정했고 마지막 28번 슬라이드에 any questions 삭제하겠습니다~"],
+                ["윤기목", "refrence 부분에도 똑같은 게 2번 들어가서 하나 지웠습니다"],
+                ["윤기목", "저 그 api call 부분에 hamming distance 부분 이해가 잘 안 가네요 ㅜㅜ 혹시 그거 담당하셨던 분 간략히 설명해주실 수 있나요"],
+                ["장건호", "저도 자세히는 찾아보지 않았었습니다 ㅠㅠㅠ 그 논문에서 원래의 것과 차이나는 정도를 hamming distance 로 나타낸다고 나와있었단것 같아요 해당 논문이 리뷰 논문이다보니 그정도만 나와있았던거 같고 찾아보려면 참고문헌 타고 들어가야할것 같아요"],
+                ["윤기목", "아하 리뷰 논문이라 설명이 자세히 나와있진 않았군요 ㅜㅜ 일단 ppt 설명대로 발표하겠습니당"],
+                ["윤기목", "저희 발표자료 klms에 올리셨나요?"], ["윤기목","수고하셨습니다"]];
+
+
+function add_enter(str, sub_string){
+    var str_n = str.length;
+    var array = str.split(sub_string);
+    var n = array.length;
+
+    var result = array[0];
+
+    if (n == 0){
+        return str;
+    }
+
+    var result = array[0];
+
+    for (var i=1; i<n; i++)
+    {
+        if(array[i]=="")
+        {
+            result = result + sub_string;
+        }
+        else
+        {
+            result = result +  sub_string +"\n" + array[i];
+        }
+    }
+
+    return result;
+}
+
+function parsing_string(input){
+    var l = input.length;
+    var rtn_value = [];
+    for (var i = 0 ; i<l ; i++){
+        var str = input[i][1];
+        if (str.length < 15)
+        {
+            rtn_value.push(input[i]);
+        }
+
+        else
+        {
+
+            var result = add_enter(str, "?");
+            result = add_enter(result, "!");
+            result = add_enter(result, ".");
+
+            result = add_enter(str, "ㅋ");
+            result = add_enter(result, "ㅎ");
+            result = add_enter(result, "ㅠ");
+            result = add_enter(result, "ㅜ");
+
+            var array = result.split("\n");
+
+            var n = array.length;
+
+            for (var j = 0; j < n; j++)
+            {
+                var token = array[j];
+
+                if( token != "")
+                {
+                    rtn_value.push([input[i][0],token]);
+                }
+            }
+         }
+     }
+
+     return rtn_value;
+
+}
