b2e2f76372787ff3160ab9ba99aff989352d5ca6
MinSOh <minsoh@kaist.ac.kr>
2019-12-06 17:08:05 +0900

    chatroom: sizing done

--- prototypes/prototype_kakaotalk/js/chatroom.js
+++ prototypes/prototype_kakaotalk/js/chatroom.js
@@ -1,6 +1,7 @@
 var current_other = false;
 var current_name = "";
 var current_id = 0;
+var shorten = false;
 
 function size_control(str){
     len = str.length;
@@ -16,8 +17,9 @@ function add_my_msg(){
     document.getElementById('msg_list').appendChild(temp);
     document.getElementById('input_msg_box').value = "";
     var temp = document.getElementById('temp');
+    temp.style.top = "0px";
     var hhh = getComputedStyle(temp).height;
-    var qqq = parseFloat(hhh)+20.0;
+    var qqq = parseFloat(hhh)+10.0;
     hhh = qqq+"px";
     document.getElementById('kkk').style.height = hhh;
     document.getElementById('temp').id = current_id;
@@ -31,14 +33,17 @@ function addMyMsgByTxt(str,who){
     if (who=="나"){
         temp.innerHTML = "<div class='my_msg' id='kkk'><div class='txt' id='temp'>"+ message +"</div></div>";
         current_other = false;
+        shorten = true;
     }else{
         if (current_other && current_name == who){
             temp.innerHTML = "<div class='other_msg' id='kkk'><div class='profile'></div><div class='txt' id='temp'>"+ message +"</div></div>";
+            shorten = true;
         }
         else{
             temp.innerHTML = "<div class='other_msg' id='kkk'><div class='profile'><img src='img/temp.jpg' alt='ProfileImage' width='40px' height='40px' /></div><div class='p_name'>"+who+"</div><div class='txt' id='temp'>"+ message +"</div></div>";
             current_other = true;
             current_name = who;
+            shorten = false;
         }
         
     }
@@ -47,6 +52,9 @@ function addMyMsgByTxt(str,who){
     var temp = document.getElementById('temp');
     var hhh = getComputedStyle(temp).height;
     var qqq = parseFloat(hhh)+30.0;
+    if (shorten){
+        temp.style.top = "20px";
+    }
     hhh = qqq+"px";
     document.getElementById('kkk').style.height = hhh;
     document.getElementById('temp').id = current_id;
@@ -57,6 +65,6 @@ function addMyMsgByTxt(str,who){
 function init(MsgsAndName){
     for(var i=0;i<MsgsAndName.length;i++){
         addMyMsgByTxt(MsgsAndName[i][1],MsgsAndName[i][0]);
-        console.log(MsgsAndName[i][0]);
+        console.log(MsgsAndName[i][1]);
     }
 }
