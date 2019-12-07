497e0013067a9750d54a6c43bf5f6b4b2e5f28c2
MinSOh <minsoh@kaist.ac.kr>
2019-12-07 23:37:44 +0900

    chatroom: modified - interval

--- prototypes/prototype_kakaotalk/js/chatroom.js
+++ prototypes/prototype_kakaotalk/js/chatroom.js
@@ -2,6 +2,7 @@ var current_other = false;
 var current_name = "";
 var current_id = 0;
 var shorten = false;
+var cnt_shorten = 0;
 
 function size_control(str){
     len = str.length;
@@ -31,19 +32,33 @@ function addMyMsgByTxt(str,who){
     var message = str;
     message = "<p style='word-break:break-all;'>"+str+"</p>";
     if (who=="나"){
+        if(current_other == false){
+            cnt_shorten = cnt_shorten + 1;
+        }
+        else{
+            cnt_shorten = 0;
+        }
         temp.innerHTML = "<div class='my_msg' id='kkk'><div class='txt' id='temp'>"+ message +"</div></div>";
         current_other = false;
         shorten = true;
     }else{
         if (current_other && current_name == who){
             temp.innerHTML = "<div class='other_msg' id='kkk'><div class='profile'></div><div class='txt' id='temp'>"+ message +"</div></div>";
-            shorten = true;
+            if (shorten){
+                cnt_shorten = cnt_shorten + 1;
+                shorten = true;
+            }
+            else{
+                shorten = true;
+                cnt_shorten = 0;
+            }
         }
         else{
             temp.innerHTML = "<div class='other_msg' id='kkk'><div class='profile'><img src='img/temp.jpg' alt='ProfileImage' width='40px' height='40px' /></div><div class='p_name'>"+who+"</div><div class='txt' id='temp'>"+ message +"</div></div>";
             current_other = true;
             current_name = who;
             shorten = false;
+            cnt_shorten = 0;
         }
         
     }
@@ -54,6 +69,11 @@ function addMyMsgByTxt(str,who){
     var qqq = parseFloat(hhh)+30.0;
     if (shorten){
         temp.style.top = "20px";
+        if (cnt_shorten>0){
+            var shorten_amount = 20 - 20*cnt_shorten;
+            var shortenshorten = shorten_amount + "px"
+            temp.style.top = shortenshorten;
+        }
     }
     hhh = qqq+"px";
     document.getElementById('kkk').style.height = hhh;
